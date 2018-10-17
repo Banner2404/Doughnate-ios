a1ff02c50872705bb4916b7cef4f56b61d1efe85
Eugene Sobol <esobol@insoftgroup.com>
2018-10-17 11:04:07 +0300

    Initial Commit

---
+++ Doughnate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Doughnate
+//
+//  Created by Евгений Соболь on 10/17/18.
+//  Copyright © 2018 Евгений Соболь. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
