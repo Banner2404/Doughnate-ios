0044fa1630b32d36a2f371f030d9692e7d302f87
Eugene Sobol <esobol@insoftgroup.com>
2018-12-01 22:06:51 +0300

    added subscription alert

--- Doughnate/View controllers/ProjectDetailsViewController.swift
+++ Doughnate/View controllers/ProjectDetailsViewController.swift
@@ -35,11 +35,38 @@ private extension ProjectDetailsViewController {
             subscriptionsStackView.removeArrangedSubview($0)
             subscriptionsStackView.removeFromSuperview()
         }
-        project.subscriptions.forEach { subscription in
-            let view = SubscriptionTypeView()
-            view.titleLabel.text = subscription.title
-            view.descriptionLabel.text = subscription.description
-            subscriptionsStackView.addArrangedSubview(view)
+        project.subscriptions.forEach {
+            subscriptionsStackView.addArrangedSubview(createSubscriptionView(for: $0))
         }
+    }
+    
+    func createSubscriptionView(for subscription: SubscriptionType) -> SubscriptionTypeView {
+        let view = SubscriptionTypeView()
+        view.titleLabel.text = subscription.title
+        view.descriptionLabel.text = subscription.description
+        
+        let tap = UITapGestureRecognizer(target: self, action: #selector(subscriptionCellTap))
+        view.addGestureRecognizer(tap)
+        return view
+    }
+    
+    @objc
+    func subscriptionCellTap(_ recognizer: UIGestureRecognizer) {
+        guard let cell = recognizer.view else { return }
+        guard let index = subscriptionsStackView.arrangedSubviews.firstIndex(of: cell) else { return }
+        let subscription = project.subscriptions[index]
+        subscribe(to: subscription)
+    }
+    
+    func subscribe(to subscription: SubscriptionType) {
+        let alert = UIAlertController(title: "Subscribe?", message: "Do you want to subscribe to $\(subscription.amount) plan?", preferredStyle: .alert)
+        let noAction = UIAlertAction(title: "No", style: .default, handler: nil)
+        let yesAction = UIAlertAction(title: "Yes", style: .default) { _ in
+            print(subscription)
+        }
+        alert.addAction(noAction)
+        alert.addAction(yesAction)
+        alert.preferredAction = yesAction
+        present(alert, animated: true, completion: nil)
     }
 }
